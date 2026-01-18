715c5269fa9d070d48e577c3f0c5911570ede866
yannleekarame <dthierry078@gmail.com>
2026-01-18 17:08:55 +0200

    Update and rename a to login.js

---
+++ login.js
@@ -0,0 +1,19 @@
+const validUsername = "admin";
+const validPassword = "1234";
+
+const form = document.getElementById("loginForm");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username === validUsername && password === validPassword){
+        alert("Login successful!");
+        window.location.href = "admin.html";
+    } else {
+        error.textContent = "Invalid username or password!";
+    }
+});
+
